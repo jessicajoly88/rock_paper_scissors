dab248483f0edd69704ba5d86214e232f2554339
Jessica Joly & Ronando Long <student@epicodus.com>
2015-08-05 12:17:44 -0700

    Initial commit first Rspec

---
+++ spec/rock_paper_scissors_spec.rb
@@ -0,0 +1,10 @@
+require('rspec')
+require('rock_paper_scissors')
+require('pry')
+require('launchy')
+
+describe('String#rock_paper_scissors') do
+it("returns true if rock is the object and scissors is the argument") do
+  expect("rock".beats?("scissors")).to(eq(true))
+ end
+end
