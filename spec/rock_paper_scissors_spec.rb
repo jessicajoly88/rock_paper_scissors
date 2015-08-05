6ec50e04bbca3f3f510e86a165a8d5cafe0fa42d
Jessica Joly & Ronando Long <student@epicodus.com>
2015-08-05 15:21:22 -0700

    Commit for second rspec, rock and paper

--- spec/rock_paper_scissors_spec.rb
+++ spec/rock_paper_scissors_spec.rb
@@ -8,5 +8,7 @@ describe('String#rock_paper_scissors') do
   it("returns true if rock is the object and scissors is the argument") do
     expect(("rock").beats?("scissors")).to(eq(true))
   end
-
+  it("returns true if rock is the object and paper is the argument") do
+    expect(("rock").beats?("paper")).to(eq(true))
+  end
 end
